2db70e692e5790314ee3296ed3247caf47eb3154
Hai Phan <hai.phan@gmail.com>
2021-04-28 11:53:18 -0700

    soundboard tool

---
+++ web/js/components.js
@@ -0,0 +1,22 @@
+
+function SoundboardNavbar() {
+
+}
+
+function SoundboardItemList() {
+    
+}
+
+function SoundboardEditItemDialog(viewRoot) {
+  this.onSubmit = function(form, action) {
+    if (!form.voice.value) return this.error = {message: "Please select a voice"}
+    if (!form.text.value.trim()) return this.error = {message: "Please enter some text"}
+    $(viewRoot).triggerHandler(action == "save" ? "save-item" : "test-item", {
+      voice: voices[form.voice.value],
+      volume: form.volume.value,
+      rate: form.rate.value,
+      pitch: form.pitch.value,
+      text: form.text.value,
+    })
+  }
+}
